5cce17193817ae9de1000825f4ff36f0e9cd0fc5
Marcos Davi <mdmalungo@gmail.com>
2024-05-25 21:51:14 +0200

    1.8: unicafe, step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,29 +9,45 @@ const App = () => {
   const Title = (props) => <h1>{props.text}</h1>
   const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
   const Static = (props) => <p>{props.text} {props.state} </p>
-  
+
+  const Statics = (props) => {
+
+    return (
+      <>
+        <Static text='good' state={props.good} />
+        <Static text='neutral' state={props.neutral} />
+        <Static text='bad' state={props.bad} />
+        <Static text='all' state={props.all} />
+        <Static text='average' state={props.average} />
+        <Static text='positive' state={props.positive} />
+      </>
+    )
+  }
+
   const handleClick = (state, setState) => {
     const setToState = () => {
-       setState(state + 1)    }
-   return setToState
+      setState(state + 1)
+    }
+    return setToState
   }
-   const all = good + bad + neutral 
-   const average = () => (good - bad) / all
-   const positive = () => (good / all) * 100 + '%'
+  const all = good + bad + neutral
+  const average = () => (good - bad) / all
+  const positive = () => (good / all) * 100 + '%'
 
   return (
     <div>
-     <Title text='give feedback' />
-     <Button handleClick={handleClick(good, setGood)} text='good' />
-     <Button handleClick={handleClick(neutral, setNeutral)} text='neutral' />
-     <Button handleClick={handleClick(bad, setBad)} text='bad' />
-     <Title text='statics' />
-     <Static text='good' state={good} />
-     <Static text='neutral' state={neutral} />
-     <Static text='bad' state={bad} />
-     <Static text='all' state={all} />
-     <Static text='average' state={average()} />
-     <Static text='positive' state={positive()} />
+      <Title text='give feedback' />
+      <Button handleClick={handleClick(good, setGood)} text='good' />
+      <Button handleClick={handleClick(neutral, setNeutral)} text='neutral' />
+      <Button handleClick={handleClick(bad, setBad)} text='bad' />
+      <Title text='statics' />
+      <Statics good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        average={average()}
+        positive={positive()}
+      />
     </div>
   )
 }
