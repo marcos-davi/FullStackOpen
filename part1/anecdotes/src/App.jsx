bb3e65f761ca6375bcdc0c1031b9457b19c80b01
Marcos Davi <mdmalungo@gmail.com>
2024-05-27 00:52:53 +0200

    1.14*: anecdotes, step 3

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,44 +1,68 @@
-import { useState } from 'react'
+import { useState } from "react";
+const Header = (props) => {
+  return <h1>{props.text}</h1>;
+};
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
 
+const Display = (props) => {
+  return (
+    <div>
+      {props.anecdotes}
+      <br />
+      has {props.points} votes
+    </div>
+  );
+};
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
-  const [selected, setSelected] = useState(0)
-  const [points, setPoints] = useState(Array(anecdotes.length).fill(0))  
+  const [selected, setSelected] = useState(0);
+  const [maxVoted, setMaxVoted] = useState(0);
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0));
 
-  const handleClick = () => {    
-  const randomNum = Math.floor(Math.random()*(anecdotes.length - 0) + 0)
-    setSelected(randomNum);    
-  }
-  const handleVote = () => {
-    const newPoints = {...points}
-    newPoints [selected] +=1;
-    setPoints(newPoints)
-  } 
-     
-  return (
-    <>    
-      <div>
-     {anecdotes[selected]}
-      <br />
-      has {points[selected]} votes
-      </div> 
-      <button onClick={handleVote}>vote</button>  
-      <button onClick={handleClick}>next anedocte</button> 
-    </>
-      
-      
+  const handleClick = () => {
+    const randomNum = Math.floor(Math.random() * (anecdotes.length - 0) + 0);
+    setSelected(randomNum);
+    calcMostVoted();
+  };
+  const calcMostVoted = () => {
+    let max = 0;
+    for (const point in points) {
+      if (points[point] > max) {
+        max = points[point];
+        setMaxVoted(point);
+      }
+    }
    
-  )
-}
+  };
+  const handleVote = () => {
+    const newPoints = { ...points };
+    newPoints[selected] += 1;
+    setPoints(newPoints);
+    calcMostVoted();
+  };
 
-export default App
+  return (
+    <>
+      <Header text="Anecdote of the day" />
+      <Display anecdotes={anecdotes[selected]} points={points[selected]} />
+      <Button handleClick={handleVote} text="vote" />
+      <Button handleClick={handleClick} text="next anedocte" />
+      <Header text="Anecdote with most votes" />
+      <Display anecdotes={anecdotes[maxVoted]} points={points[maxVoted]} />
+    </>
+  );
+};
+
+export default App;
