0e7b137db5fc5622f066d04f8da92a094d85220b
Marcos Davi <mdmalungo@gmail.com>
2024-06-10 13:36:44 +0200

    2.9: phonebook, step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,31 +2,26 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: '00346762345' },
-    { name: "José Padilha", number: '003467633445'},
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]);
 
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
-  const Persons = ({ persons }) => {
-    return (
-      <div>
-        {persons.map((person) => (
+      
 
-          <div key={person.name}>{person.name} {person.number}</div>
-        ))}
-      </div>
-    );
-  };
   const handleNoteChangeName = (event) => {
-    setNewName(event.target.value);
-    
+    setNewName(event.target.value);    
   };
-  const handleNoteChangeNumber = (event) => {
-    
+  const handleNoteChangeNumber = (event) => {    
     setNewNumber(event.target.value);
   };
+
+  
   const addPerson = (event) => {
     event.preventDefault();
     const allPerson = persons.map((person) => person.name)
@@ -41,9 +36,22 @@ const App = () => {
     }
   };
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+  const personsList = filter === ''
+    ? persons
+    : persons.filter(person =>
+        person.name.toLowerCase().includes(filter.toLowerCase()))
+  const listNames = () => personsList.map(person => 
+    <p key={person.name}>{person.name} {person.number}</p>
+  )
+  
   return (
     <div>
       <h2>Phonebook</h2>
+      <p>filter show with<input value={filter} onChange={handleFilterChange}></input></p>
+      <h2>add a new</h2>
       <form onSubmit={addPerson} method="POST">
         <div>
           name: <input value={newName} onChange={handleNoteChangeName} />
@@ -56,7 +64,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Persons persons={persons} />
+      <>{listNames()}</>
     </div>
   );
 };
